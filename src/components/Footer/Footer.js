d2d9a4f37af5ad77cb3cfbd6ecedd3496c60d315
Imre Mehesz <imehesz@gmail.com>
2024-10-20 12:39:06 -0400

    Added static index link for SEO

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,6 +9,7 @@ function Footer() {
           <img src="/bizzbuzzcomics-logo__light.svg" title="BizzBuzzComics.com" className="logo" />
         </div>
         <span className="">© 2014-2024 BizzBuzz Comics by SAD BOI WORKS</span>
+        <span class="lnk__index-static"><a href="./index-static.html">index-static.html</a></span>
       </div>
     </footer>
   );
